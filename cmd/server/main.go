1cb73fb83be41e6b0471b23481856a4482433625
Raphael Fua <raphael.fua@epfl.ch>
2026-08-20 11:35:38 +0200

    Ch2: Message Brokers - L3: Connect - Part 2

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	// connectionString := "amqp://guest:guest@127.0.0.1:5672/"
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	connectionPointer, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer connectionPointer.Close()
+
 	fmt.Println("Starting Peril server...")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	
+	fmt.Println("\nClosing Peril server...")
 }
+
+
+
